efaef8799e1de8206141684a4abe62f3f9cb7e64
chichardanthony <chichardanthony@gmail.com>
2025-01-06 16:10:03 -0700

    Update main.js

--- electron/main.js
+++ electron/main.js
@@ -1,1 +1,31 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  // For development
+  if (process.env.NODE_ENV === 'development') {
+    win.loadURL('http://localhost:5173');
+    win.webContents.openDevTools();
+  } else {
+    // For production
+    win.loadFile(path.join(__dirname, '../dist/index.html'));
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
