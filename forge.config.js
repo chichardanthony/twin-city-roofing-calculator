63a5d412fc6983754bc39f0e7b427008ee9b77d9
chichardanthony <chichardanthony@gmail.com>
2025-01-06 16:11:10 -0700

    Update forge.config.js

--- forge.config.js
+++ forge.config.js
@@ -1,1 +1,27 @@
-
+module.exports = {
+  packagerConfig: {
+    name: "Twin City Roofing Calculator",
+    executableName: "twin-city-roofing-calculator",
+    icon: "./src/assets/icon" // Add your icon path
+  },
+  makers: [
+    {
+      name: "@electron-forge/maker-squirrel",
+      config: {
+        name: "twin_city_roofing_calculator"
+      }
+    },
+    {
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin", "win32"]
+    },
+    {
+      name: "@electron-forge/maker-deb",
+      config: {}
+    },
+    {
+      name: "@electron-forge/maker-rpm",
+      config: {}
+    }
+  ]
+};
