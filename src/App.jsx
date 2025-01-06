2967026648c33c34ab4f7c2b33d77d262dd8045f
chichardanthony <chichardanthony@gmail.com>
2025-01-06 16:17:20 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,13 @@
+import React from 'react'
+import RoofingCalculator from './components/RoofingCalculator'
+import './App.css'
 
+function App() {
+  return (
+    <div className="App">
+      <RoofingCalculator />
+    </div>
+  )
+}
+
+export default App
