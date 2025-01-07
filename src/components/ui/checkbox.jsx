633c1cd5f1b9d5c514822c9ca1ea5054c1c95078
chichardanthony <chichardanthony@gmail.com>
2025-01-07 15:12:58 -0700

    Update checkbox.jsx

--- src/components/ui/checkbox.jsx
+++ src/components/ui/checkbox.jsx
@@ -1,8 +1,7 @@
 import * as React from "react"
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox"
-import { Check } from "lucide-react"
 import { cn } from "@/lib/utils"
-
+ 
 const Checkbox = React.forwardRef(({ className, ...props }, ref) => (
   <CheckboxPrimitive.Root
     ref={ref}
@@ -11,14 +10,8 @@ const Checkbox = React.forwardRef(({ className, ...props }, ref) => (
       className
     )}
     {...props}
-  >
-    <CheckboxPrimitive.Indicator
-      className={cn("flex items-center justify-center text-current")}
-    >
-      <Check className="h-4 w-4" />
-    </CheckboxPrimitive.Indicator>
-  </CheckboxPrimitive.Root>
+  />
 ))
 Checkbox.displayName = CheckboxPrimitive.Root.displayName
-
+ 
 export { Checkbox }
