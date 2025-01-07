8ff1c713491506f544a962c6e4b752423cf6ba9f
chichardanthony <chichardanthony@gmail.com>
2025-01-07 15:08:32 -0700

    Update index.js

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,6 +1,6 @@
-export { Card, CardHeader, CardTitle, CardContent } from "./card";
-export { Input } from "./input";
-export { Label } from "./label";
-export { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./select";
-export { Checkbox } from "./checkbox";
-export { Separator } from "./separator";
+export * from "./card"
+export * from "./input"
+export * from "./label"
+export * from "./select"
+export * from "./separator"
+export * from "./checkbox"
