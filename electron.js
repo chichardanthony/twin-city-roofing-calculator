4499612a9997528853eba027a3c983c540f22fe8
chichardanthony <chichardanthony@gmail.com>
2025-01-07 08:32:11 -0700

    Update electron.js

--- electron.js
+++ electron.js
@@ -1,1 +1,30 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+
+  // Load the app in development or production
+  if (process.env.NODE_ENV === 'development') {
+    win.loadURL('http://localhost:5173');
+    win.webContents.openDevTools();
+  } else {
+    win.loadFile(path.join(__dirname, 'dist/index.html'));
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
