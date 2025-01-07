fdfed7f69a20db9486c9ad67c141c5e687ff81df
chichardanthony <chichardanthony@gmail.com>
2025-01-07 14:20:27 -0700

    Update AdditionalComponentsForm.jsx

--- src/components/AdditionalComponentsForm.jsx
+++ src/components/AdditionalComponentsForm.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { 
-  Label, 
-  Input 
-} from "@/components/ui";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
 
 const AdditionalComponentsForm = ({ inputs, onInputChange }) => {
   return (
