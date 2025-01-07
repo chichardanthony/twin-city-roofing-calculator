87bcaa44d718c88b10900728e13fe2469183ec20
chichardanthony <chichardanthony@gmail.com>
2025-01-07 15:18:35 -0700

    Update utils.js

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,13 +1,6 @@
-Run npm run build
-
-> twin-city-roofing-calculator@1.0.0 build
-> vite build
-
-vite v4.5.5 building for production...
-transforming...
-[vite:load-fallback] Could not load D:\a\twin-city-roofing-calculator\twin-city-roofing-calculator\src/lib/utils (imported by src/components/ui/card.jsx): ENOENT: no such file or directory, open 'D:\a\twin-city-roofing-calculator\twin-city-roofing-calculator\src\lib\utils'
-✓ 36 modules transformed.
-error during build:
-Error: Could not load D:\a\twin-city-roofing-calculator\twin-city-roofing-calculator\src/lib/utils (imported by src/components/ui/card.jsx): ENOENT: no such file or directory, open 'D:\a\twin-city-roofing-calculator\twin-city-roofing-calculator\src\lib\utils'
-✓ built in 598ms
-Error: Process completed with exit code 1.
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+ 
+export function cn(...inputs) {
+  return twMerge(clsx(inputs));
+}
